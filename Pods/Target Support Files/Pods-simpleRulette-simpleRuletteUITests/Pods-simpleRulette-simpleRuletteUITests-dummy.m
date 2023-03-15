28218ae17b8509b8333d644369ebc6d440f9289d
kazukiomori <kazuki1110170501@gmail.com>
2023-03-15 11:32:39 +0900

    chartsを追加

---
+++ Pods/Target Support Files/Pods-simpleRulette-simpleRuletteUITests/Pods-simpleRulette-simpleRuletteUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_simpleRulette_simpleRuletteUITests : NSObject
+@end
+@implementation PodsDummy_Pods_simpleRulette_simpleRuletteUITests
+@end
