c666ad5325791bbd31c4c7781a7a3cd6b87e0888
kazukiomori <kazuki1110170501@gmail.com>
2023-03-15 15:37:20 +0900

    ruletteViewControllerにタイトルと矢印と円グラフを追加

--- simpleRulette/RuletteViewController.swift
+++ simpleRulette/RuletteViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 import Charts
 
 class RuletteViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var triangleImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
