fcd7549e12cdf2faee599184d7b9f411e73e819c
kazukiomori <kazuki1110170501@gmail.com>
2023-03-17 09:56:12 +0900

    RuletteTableViewCellを作成

---
+++ simpleRulette/View/TableView/RuletteTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RuletteTableViewCell.swift
+//  simpleRulette
+//
+//  Created by Kazuki Omori on 2023/03/17.
+//
+
+import UIKit
+
+class RuletteTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
