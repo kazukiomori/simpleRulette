7a9568d7246abbae43655fced21eb404bdbe8801
kazukiomori <kazuki1110170501@gmail.com>
2023-03-21 03:44:27 +0900

    TemplateViewControllerのUIを作成

--- simpleRulette/View/ViewController/TemplateViewController.swift
+++ simpleRulette/View/ViewController/TemplateViewController.swift
@@ -8,9 +8,23 @@
 import Foundation
 import UIKit
 
-class TemplateViewController: UIViewController {
+class TemplateViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var bannerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
