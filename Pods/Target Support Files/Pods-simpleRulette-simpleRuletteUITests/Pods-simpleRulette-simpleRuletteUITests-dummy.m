3b9c3b9b82045f2fc7bf6583ef20e61267ceb106
kazukiomori <kazuki1110170501@gmail.com>
2026-05-24 23:03:32 +0900

    pods関連のファイルを削除

--- Pods/Target Support Files/Pods-simpleRulette-simpleRuletteUITests/Pods-simpleRulette-simpleRuletteUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_simpleRulette_simpleRuletteUITests : NSObject
-@end
-@implementation PodsDummy_Pods_simpleRulette_simpleRuletteUITests
-@end
